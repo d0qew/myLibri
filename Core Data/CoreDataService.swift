c6148bc3974a0f88f6a6a2e4ceb4feb93861ae8a
Danya <dkonashenko7@gmail.com>
2023-05-10 19:52:40 +0300

    -fix assets

--- Core Data/CoreDataService.swift
+++ Core Data/CoreDataService.swift
@@ -2,7 +2,7 @@
 //  CoreDataService.swift
 //  myLibri
 //
-//  Created by Daniil Konashenko on 06.05.2023.
+//  Created by Daniil Konashenko on 10.05.2023.
 //
 
 import UIKit
@@ -11,10 +11,11 @@ import CoreData
 class CoreDataService{
     static let shared = CoreDataService()
     
-    // MARK: - Core Data stack
+// MARK: - Core Data stack
+    
     lazy var persistentContainer: NSPersistentContainer = {
         
-        let container = NSPersistentContainer(name: "BookSaved")
+        let container = NSPersistentContainer(name: "SavedBook")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 fatalError("Unresolved error \(error), \(error.userInfo)")
@@ -23,7 +24,7 @@ class CoreDataService{
         return container
     }()
     
-    // MARK: - Core Data Saving support
+// MARK: - Core Data Saving support
     func saveContext () {
         let context = persistentContainer.viewContext
         if context.hasChanges {
@@ -31,8 +32,8 @@ class CoreDataService{
                 try context.save()
             } catch {
                 context.rollback()
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
             }
         }
     }
