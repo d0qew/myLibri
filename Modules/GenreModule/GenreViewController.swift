d3426477634d97fbe29a4215f2f28f2a76ece720
d0qew <dkonashenko7@gmail.com>
2023-06-17 17:51:09 +0300

    refactoring: improved readability GenreViewController
    * and btw, remade casting cell

--- Modules/GenreModule/GenreViewController.swift
+++ Modules/GenreModule/GenreViewController.swift
@@ -31,7 +31,7 @@ class GenreViewController: UIViewController {
     }
 }
 
-// MARK: - Setup spinner
+//  MARK: - Setup spinner
 extension GenreViewController {
     func setupSpinner() {
         view.addSubview(spinner)
@@ -44,13 +44,12 @@ extension GenreViewController {
     }
 }
 
-// MARK: - GenreViewControllerProtocol
+//  MARK: - GenreViewControllerProtocol
 extension GenreViewController: GenreViewControllerProtocol {
     func updateCollectionView(with books: BooksModel) {
         stopSpinner()
-        
         set(with: books)
-            self.configureCollectionView()
+        configureCollectionView()
     }
     
     func updateView(with title: String) {
@@ -65,8 +64,10 @@ extension GenreViewController {
         let layout = UICollectionViewFlowLayout()
         layout.sectionInset = UIEdgeInsets(top: 0, left: 15, bottom: 0, right: 15)
         layout.scrollDirection = .vertical
-        layout.itemSize = CGSize(width: Double(ScreenSize.shared().screenWidth * 0.45),
-                                 height: Double(ScreenSize.shared().screenWidth * 0.82))
+        layout.itemSize = CGSize(
+            width: Double(ScreenSize.shared().screenWidth * 0.45),
+            height: Double(ScreenSize.shared().screenWidth * 0.82)
+        )
         layout.minimumLineSpacing = 10
         layout.minimumInteritemSpacing = 0
         return layout
@@ -76,7 +77,10 @@ extension GenreViewController {
 //  MARK: - Setup Collection View
 extension GenreViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     private func configureCollectionView() {
-        collectionView = UICollectionView(frame: .zero, collectionViewLayout: createLayout())
+        collectionView = UICollectionView(
+            frame: .zero,
+            collectionViewLayout: createLayout()
+        )
         collectionView.delegate = self
         collectionView.dataSource = self
         
@@ -85,8 +89,10 @@ extension GenreViewController: UICollectionViewDelegate, UICollectionViewDataSou
         collectionView.showsHorizontalScrollIndicator = false
         collectionView.showsVerticalScrollIndicator = false
         
-        collectionView.register(BooksCollectionViewCell.self,
-                                forCellWithReuseIdentifier: BooksCollectionViewCell.reuseId)
+        collectionView.register(
+            BooksCollectionViewCell.self,
+            forCellWithReuseIdentifier: BooksCollectionViewCell.reuseId
+        )
         
         view.addSubview(collectionView)
         collectionView.snp.makeConstraints { make in
@@ -103,8 +109,13 @@ extension GenreViewController: UICollectionViewDelegate, UICollectionViewDataSou
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: BooksCollectionViewCell.reuseId,
-                                                      for: indexPath) as! BooksCollectionViewCell
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: BooksCollectionViewCell.reuseId,
+            for: indexPath
+        ) as? BooksCollectionViewCell else {
+            fatalError("Can't create new cell")
+        }
+        
         cell.title.text = cells.books[indexPath.row].name
         cell.authors.text = cells.books[indexPath.row].authors[0].first_name + " " + cells.books[indexPath.row].authors[0].last_name
         cell.imageView.image = cells.images[cells.books[indexPath.row].id]
@@ -115,6 +126,7 @@ extension GenreViewController: UICollectionViewDelegate, UICollectionViewDataSou
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         collectionView.deselectItem(at: indexPath, animated: true)
         let infoBook = cells.books[indexPath.row]
+        
         presenter?.didSelectItem(with: infoBook)
     }
     
