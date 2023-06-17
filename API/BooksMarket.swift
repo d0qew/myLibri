51644c888ed1858015fef698f0d597bcd7b4c3db
d0qew <dkonashenko7@gmail.com>
2023-06-17 23:24:20 +0300

    refactoring: improved readability BooksMarket
    * add func setToken(..)
    * remade creating url and string for url

--- API/BooksMarket.swift
+++ API/BooksMarket.swift
@@ -10,7 +10,6 @@ import UIKit
 
 public final class BooksMarket {
     private static var instance: BooksMarket?
-    
     static func shared() -> BooksMarket {
         if instance == nil {
             instance = BooksMarket()
@@ -18,19 +17,56 @@ public final class BooksMarket {
         return instance!
     }
     
-    private let token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJkMHFldyIsImlhdCI6MTY4NTg5OTQ2NCwiZXhwIjoxNjg1OTQyNjY0fQ.JTEy710Yl80Wxe--kpEzwZ0w8q8L3K6SHERV8GguJB8"
     var imageCache = NSCache<NSString, UIImage>()
     
-    func getBooks(with idGenre: Int) async throws -> Books? {
-        guard let url = URL(string: "http://localhost:8080/api/books?page=0&size=7&genre_ids=\(idGenre)") else {
-            return nil
-        }
-        
-        var request = URLRequest(url: url)
+    private let token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJkMHFldyIsImlhdCI6MTY4NzAwNjM2MiwiZXhwIjoxNjg3MDQ5NTYyfQ.CeWSIzjqojRWKhh5nqEJcWxH944LqF_HWbeR3PmCH3s"
+    
+    private func setToken(for request: inout URLRequest, token: String) {
         request.setValue(
             "Bearer \(token)",
             forHTTPHeaderField: "Authorization"
         )
+    }
+}
+//  MARK: - Download Genres
+internal extension BooksMarket {
+    func getGeners() async throws -> Genres? {
+        let urlString = EndpointPath.genresDownload.url
+        guard let url = URL(string: urlString) else {
+            return nil
+        }
+        
+        var request = URLRequest(url: url)
+        setToken(for: &request, token: token)
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(for: request)
+            let genreJSON = try JSONDecoder().decode(Genres.self, from: data)
+            return genreJSON
+        } catch {
+            print("--Error JSON decode \(error.localizedDescription)")
+            return nil
+        }
+    }
+}
+
+//  MARK: - Download Books
+internal extension BooksMarket {
+    func getBooks(with idGenre: Int) async throws -> Books? {
+        let urlString = EndpointPath.booksDownload.url
+        guard var urlComponents = URLComponents(string: urlString) else {
+            return nil
+        }
+        urlComponents.queryItems = [
+            URLQueryItem(name: "page", value: "0"),
+            URLQueryItem(name: "size", value: "7"),
+            URLQueryItem(name: "genre_ids", value: String(idGenre))
+        ]
+        guard let url = urlComponents.url else {
+            return nil
+        }
+        var request = URLRequest(url: url)
+        setToken(for: &request, token: token)
         
         do {
             let (data, _) = try await URLSession.shared.data(for: request)
@@ -41,60 +77,42 @@ public final class BooksMarket {
             return nil
         }
     }
-    
-    func getGeners() async throws -> Genres? {
-        guard let url = URL(string: "http://localhost:8080/api/genres") else {
-            return nil
-        }
-        var request = URLRequest(url: url)
-        request.setValue(
-            "Bearer \(token)",
-            forHTTPHeaderField: "Authorization"
-        )
-        do {
-            let (data, _) = try await URLSession.shared.data(for: request)
-            let genreJSON = try JSONDecoder().decode(Genres.self, from: data)
-            return genreJSON
-        } catch {
-            print("--Error JSON decode \(error.localizedDescription)")
-            return nil
-        }
-    }
-    
+}
+
+//  MARK: - Download Images
+internal extension BooksMarket {
     func getImage(idBook: Int) async throws -> UIImage? {
-        guard let url = URL(string: "http://localhost:8080/api/binary-content/\(idBook)/image") else {
-            return UIImage(named: "book")
+        let urlString = String(format: EndpointPath.imageDownload.url, String(idBook))
+        guard let url = URL(string: urlString) else {
+            return nil
         }
-        // check cached image
         if let cachedImage = imageCache.object(forKey: url.absoluteString as NSString) {
             return cachedImage
         } else {
-            
             var request = URLRequest(url: url)
-            request.setValue(
-                "Bearer \(token)",
-                forHTTPHeaderField: "Authorization"
-            )
+            setToken(for: &request, token: token)
             
             let (data, _) = try await URLSession.shared.data(for: request)
             if let image = UIImage(data: data){
                 self.imageCache.setObject(image, forKey: url.absoluteString as NSString)
                 return image
             }else {
-                return UIImage(named: "book")
+                return nil
+                
             }
         }
     }
-    
+}
+
+//  MARK: - Download Book's Data
+internal extension BooksMarket {
     func dowloadBook(idBook: Int) async throws -> Data? {
-        guard let url = URL(string: "http://localhost:8080/api/binary-content/\(idBook)/raw") else {
+        let urlString = String(format: EndpointPath.dataBookDownload.url, String(idBook))
+        guard let url = URL(string: urlString) else {
             return nil
         }
         var request = URLRequest(url: url)
-        request.setValue(
-            "Bearer \(token)",
-            forHTTPHeaderField: "Authorization"
-        )
+        setToken(for: &request, token: token)
         
         let (data, _) = try await URLSession.shared.data(for: request)
         return data
