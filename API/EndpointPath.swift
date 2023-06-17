4946af1f6db8ccd8477988fbf4b4909bb13b09d1
d0qew <dkonashenko7@gmail.com>
2023-06-17 23:23:11 +0300

    create: add new enum EndpointPath for urls

--- API/EndpointPath.swift
+++ API/EndpointPath.swift
@@ -7,4 +7,22 @@
 
 import Foundation
 
-enum EndpointPath
+enum EndpointPath {
+    case genresDownload
+    case booksDownload
+    case imageDownload
+    case dataBookDownload
+    
+    var url: String {
+        switch self {
+        case .genresDownload:
+            return "http://localhost:8080/api/genres"
+        case .booksDownload:
+            return "http://localhost:8080/api/books"
+        case .imageDownload:
+            return "http://localhost:8080/api/binary-content/%@/image"
+        case .dataBookDownload:
+            return "http://localhost:8080/api/binary-content/%@/raw"
+        }
+    }
+}
