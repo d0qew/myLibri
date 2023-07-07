527771f39635bbf431deff73f403e3c199ded6f0
d0qew <dkonashenko7@gmail.com>
2023-07-07 23:14:31 +0300

    update: add 2 enums with title and message for AlertController

---
+++ myLibri/Entities/AlertMessage.swift
@@ -0,0 +1,12 @@
+//
+//  AlertMessage.swift
+//  myLibri
+//
+//  Created by Daniil Konashenko on 07.07.2023.
+//
+
+import Foundation
+
+enum AlertMessage: String {
+    case noInternetConnection = "No connection to the server, please check your internet connection"
+}
---
+++ myLibri/Entities/AlertTitle.swift
@@ -0,0 +1,12 @@
+//
+//  AlertTitle.swift
+//  myLibri
+//
+//  Created by Daniil Konashenko on 07.07.2023.
+//
+
+import Foundation
+
+enum AlertTitle: String {
+    case noInternetConnection = "No Internet Connection "
+}
